42f98e17cce866e445f2c347d4a56927506c8d74
Kunven <kunven@hotmail.es>
2024-01-03 01:49:00 -0500

    Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Topbar/>
       <div className="bg-gradient-to-r from-red-600 to-orange-500 m-5">
         <Card id="home">          
-          <CardBody className="grid grid-cols-2  content-center max-w-5xl mx-auto px-8 justify-center h-full">
+          <CardBody className="grid grid-cols-2  content-center max-w-full mx-auto px-8 justify-center h-full">
             <div className="align-middle my-20">
               <p className="text-6xl">Hello! My name is <span className="text-red-600">Luis</span> And I'm a FullStack Developer!</p>
               <p className="text-3xl mt-5">I made this website as a way of showing my skills. I've developed web, desktop and mobile applications with various technologies over the years. I've also used cloud technologies like AWS and Google Cloud</p>
