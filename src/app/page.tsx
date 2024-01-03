e70ba50f382eeafbcd67b6022060093917ee4984
Luis Roberto Andrade Chun <90422643+Kunven@users.noreply.github.com>
2024-01-03 17:10:11 -0500

    Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Topbar/>
       <div className="bg-gradient-to-r from-red-600 to-orange-500 m-5">
         <Card id="home">          
-          <CardBody className="grid grid-cols-2  content-center max-w-full mx-auto px-8 justify-center h-full">
+          <CardBody className="grid grid-cols-2  content-center max-w-7xl mx-auto px-8 justify-center h-full">
             <div className="align-middle my-20">
               <p className="text-6xl">Hello! My name is <span className="text-red-600">Luis</span> And I'm a FullStack Developer!</p>
               <p className="text-3xl mt-5">I made this website as a way of showing my skills. I've developed web, desktop and mobile applications with various technologies over the years. I've also used cloud technologies like AWS and Google Cloud</p>
@@ -44,8 +44,11 @@ export default function Home() {
           </CardBody>
         </Card>
         <Card className="mt-5" id="aboutMe">          
-          <CardBody className="mx-32 my-20">
-          <p className="text-6xl underline underline-offset-8 decoration-red-600">About me</p>          
+          <CardBody className="content-center max-w-7xl mx-auto px-8 justify-center h-full">
+            <p className="text-6xl underline underline-offset-8 decoration-red-600">About me</p>  
+            <div className="grid grid-cols-2  content-center mx-auto justify-center h-full">
+              <p>I'm a very passionate developer. I have been tinkering with computers and software since basically forever, so I basically love what I do. That love and passion allows me to be very detailed when I write code to the company that I'm working for.</p>
+            </div>        
           </CardBody>
         </Card>
       </div>      
